0d30c5b2c68cfbdb059efced424db743ba2c2731
Martin Facorro <mfacorro@live.com.ar>
2023-03-15 22:52:00 -0300

    add functions to read how much words have the string

--- FirstLetterToTheEnd/Program.cs
+++ FirstLetterToTheEnd/Program.cs
@@ -4,7 +4,25 @@
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      /*
+       * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+      Examples
+
+       * 
+       */
+      Console.WriteLine(MoveToEnd("Pig latin is cool")); // igPay atinlay siay oolcay
+      Console.WriteLine(MoveToEnd("Hello world !"));     // elloHay orldway !
+    }
+
+    public static string MoveToEnd(string pParam)
+    {
+      var NumberOfWords = CountWordsInString(pParam);
+      return "";
+    }
+
+    private static int CountWordsInString(string pParam)
+    { 
+    
     }
   }
 }
